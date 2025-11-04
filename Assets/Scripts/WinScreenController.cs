734c309a4c4f52f54d3c3b1dbf43d42da9326801
jw-real <j.wayne.real@proton.me>
2025-11-04 12:26:31 -0600

    Added Win/Lose Sprites to WinScreenController

--- Assets/Scripts/WinScreenController.cs
+++ Assets/Scripts/WinScreenController.cs
@@ -1,8 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement; // for scene loading
+using UnityEngine.UI; // for display of winning images and sounds
 
 public class WinScreenController : MonoBehaviour
 {
+    public Image winnerImage;           // UI Image component
+    public Sprite playerWinSprite;      // assign in Inspector
+    public Sprite computerWinSprite;    // assign in Inspector
+
+    public AudioSource audioSource;     // attach an AudioSource component
+    public AudioClip playerWinClip;     // assign in Inspector
+    public AudioClip computerWinClip;   // assign in Inspector
+
+    private void Start()
+    {
+        GameManager gm = FindObjectOfType<GameManager>();
+
+        // Display winner image
+        if (gm.gameWinner == GameManager.Winner.Player)
+        {
+            winnerImage.sprite = playerWinSprite;
+            audioSource.clip = playerWinClip;
+        }
+        else if (gm.gameWinner == GameManager.Winner.Computer)
+        {
+            winnerImage.sprite = computerWinSprite;
+            audioSource.clip = computerWinClip;
+        }
+
+        // Play the audio
+        audioSource.Play();
+    }
+
     // Called when the "New Game" button is pressed
     public void NewGame()
     {
